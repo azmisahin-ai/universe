77a43cf124b4b4a429e24b09aa803aa9a51d6cfe
Azmi SAHIN <azmisahin@outlook.com>
2024-04-16 00:33:04 +0000

    ✨: kernel memory address

--- src/os/boot.asm
+++ src/os/boot.asm
@@ -8,31 +8,27 @@ org 0x7C00  ; Kodun başlangıç adresini belirliyoruz
     mov ds, ax         ; DS'yi sıfıra ayarla
 
 Main16:
-    ; Ekrana çıktı basmak için gerekli fonksiyonları çağırıyoruz
-    call PrintMessage
+    
+    call PrintMessage ; Ekrana çıktı basmak için gerekli fonksiyonları çağırıyoruz
 
-    ; Diskten kernel dosyasını yükleme işlemleri
+disk:    ; Diskten kernel dosyasını yükleme işlemleri
     mov ah, 0x02       ; Disk okuma fonksiyonu
     mov al, 1          ; Okunacak sektör sayısı (kernel için 1 sektör yeterlidir)
     mov ch, 0          ; Silindir (başlangıç)
     mov cl, 2          ; Sector numarası (1. sektör)
     mov dh, 0          ; Başlangıç başlığı
     mov dl, 0x80       ; 0x80 - İlk sabit disk
-    mov bx, 0x1000     ; Yükleyeceğimiz bellek adresi: 0x1000'den itibaren
-    ; mov bx, 0x0000  ; Bellek adresi (0x0000:0x7E00
+    mov bx, 0x0000     ; Yükleyeceğimiz bellek adresi: 0x0000:7E00
     int 0x13           ; Disk okuma çağrısı
 
     jc disk_error      ; Hata durumunda disk_error işaretçisine git
 
-    jmp 0x1000:0000    ; Kernel'in başlangıç adresine atla (0x1000:0000)
-    ; İşletim sistemi kernel'ini başlat
-    ; jmp 0x0000:0x7E00  ; Kernel'in başlangıç adresine atla
+    jmp 0x0000:7E00    ; Kernel'in başlangıç adresine atla
 
 done:
     ret
 
-; Yazıyı ekrana yazdıran alt fonksiyon
-PrintString:
+PrintString: ; Yazıyı ekrana yazdıran alt fonksiyon
     lodsb           ; SI'den bir karakter yükle
     or al, al       ; AL'nin 0 olup olmadığını kontrol et
     jz done         ; Eğer AL 0 ise yazma işlemi bitmiştir
@@ -41,21 +37,19 @@ PrintString:
     int 0x10        ; BIOS'a karakter yazdırma çağrısı
     jmp PrintString ; Bir sonraki karakter için tekrarla
 
-
-; Ekrana mesaj yazdıran fonksiyon
-PrintMessage:
+PrintMessage: ; Ekrana mesaj yazdıran fonksiyon
     mov si, bootMessage  ; Yazdırılacak mesajın adresini SI'ye yükle
     call PrintString     ; Yazıyı ekrana yazdıran alt fonksiyonu çağır
     ret
 
-disk_error:
-    ; Hata durumunda ekrana bir mesaj yaz ve işlemi durdur
+disk_error:     ; Hata durumunda ekrana bir mesaj yaz ve işlemi durdur
+
     mov si, diskErrorMessage
     call PrintString
     jmp $ ;  durdur
 
 bootMessage db 'Booting!', 0  ; Yazdırılacak mesajı
-diskErrorMessage db 'Disk okuma hatası!', 0
+diskErrorMessage db 'Disk reading error!', 0
 
 times 510-($-$$) db 0          ; Boot sektörü boyutunu 512 byte'a tamamla
 dw 0xAA55                      ; Boot sektörü imzası
--- src/os/kernel.asm
+++ src/os/kernel.asm
@@ -2,20 +2,19 @@
 
 BITS 16  ; 16-bit modda başlıyoruz
 
-; ORG 0x7E00  ; Kernel'in bellek adresi
+org 0x7E00  ; Kernel'in bellek adresi
 
 Start:
     mov ax, 0x07C0   ; DS'yi başlangıç adresiyle ayarla
     mov ds, ax
 
-    ; Ekrana "Hello, Universe!" yazdır
+
     mov si, helloMessage  ; Yazdırılacak mesajın adresini SI'ye yükle
     call PrintString       ; Yazıyı ekrana yazdıran fonksiyonu çağır
 
-    ; Sonsuz döngü
-    jmp $
+    
+    jmp $ ; Sonsuz döngü
 
-; Yazıyı ekrana yazdıran fonksiyon
 PrintString:
     lodsb           ; SI'den bir karakter yükle
     or al, al       ; AL'nin 0 olup olmadığını kontrol et
@@ -29,5 +28,3 @@ done:
     ret
 
 helloMessage db 'Hello, Universe!', 0  ; Yazdırılacak mesajı
-
-;times 512-($-$$) db 0  ; 512 byte boyutunda bellek alanı ayır
