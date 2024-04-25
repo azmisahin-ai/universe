cebcac8bdba890471e9d100662338e406222e21f
Azmi SAHIN <azmisahin@outlook.com>
2024-04-25 14:39:02 +0300

    ♻️: change folder

--- .docker/build.sh
+++ .docker/build.sh
@@ -10,25 +10,25 @@ ARCH="$1"
 case "$ARCH" in
     linux_64)
         mkdir -p "$PROJECT_FOLDER/build/release/$ARCH"
-        nasm -F dwarf -g -f elf64 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.o" -D"$ARCH" "$PROJECT_FOLDER/src/universe.asm"
+        nasm -F dwarf -g -f elf64 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.o" -D"$ARCH" "$PROJECT_FOLDER/src/universe/universe.asm"
         ld -m elf_x86_64 -o "$PROJECT_FOLDER/build/release/$ARCH/universe" "$PROJECT_FOLDER/build/release/$ARCH/universe.o"
         echo "Linux 64-bit build completed successfully."
         ;;
     linux_32)
         mkdir -p "$PROJECT_FOLDER/build/release/$ARCH"
-        nasm -F dwarf -g -f elf32 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.o" -D"$ARCH" "$PROJECT_FOLDER/src/universe.asm"
+        nasm -F dwarf -g -f elf32 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.o" -D"$ARCH" "$PROJECT_FOLDER/src/universe/universe.asm"
         ld -m elf_i386 -o "$PROJECT_FOLDER/build/release/$ARCH/universe" "$PROJECT_FOLDER/build/release/$ARCH/universe.o"
         echo "Linux 32-bit build completed successfully."
         ;;
     win_64)
         mkdir -p "$PROJECT_FOLDER/build/release/$ARCH"
-        nasm -f win64 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.obj" -D"$ARCH" "$PROJECT_FOLDER/src/universe.asm"
+        nasm -f win64 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.obj" -D"$ARCH" "$PROJECT_FOLDER/src/universe/universe.asm"
         i686-w64-mingw32-ld -o "$PROJECT_FOLDER/build/release/$ARCH/universe.exe" "$PROJECT_FOLDER/build/release/$ARCH/universe.obj"
         echo "Windows 64-bit build completed successfully."
         ;;
     win_32)
         mkdir -p "$PROJECT_FOLDER/build/release/$ARCH"
-        nasm -f win32 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.obj" -D"$ARCH" "$PROJECT_FOLDER/src/universe.asm"
+        nasm -f win32 -o "$PROJECT_FOLDER/build/release/$ARCH/universe.obj" -D"$ARCH" "$PROJECT_FOLDER/src/universe/universe.asm"
         i686-w64-mingw32-ld -o "$PROJECT_FOLDER/build/release/$ARCH/universe.exe" "$PROJECT_FOLDER/build/release/$ARCH/universe.obj"
         echo "Windows 32-bit build completed successfully."
         ;;
