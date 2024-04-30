e11b0dee599f7099fd20658726905e750cd60c0e
Azmi SAHIN <azmisahin@outlook.com>
2024-04-30 04:48:30 +0300

    🚧: generated

---
+++ src/cells/generated.asm
@@ -0,0 +1,12 @@
+%ifndef generated.asm
+%define generated.asm
+
+; @file generated.asm
+; @description Mikro Organizmanın oluşturduğu kodlar
+; @author Microorganism
+; @version 0.0.0.1
+; --------------------------------------------------;--------------------------------------------------
+
+
+; ==================================================;==================================================
+%endif
